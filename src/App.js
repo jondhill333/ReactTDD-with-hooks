c0dc955dac579919c19d4f970456d65abc7bdb4f
Jon Hill <jondhill1984@gmail.com>
2021-03-03 11:53:26 +0200

    Add hooks to app for getSecretWord and test to run on app mount

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
-import logo from "./logo.svg";
+import hookActions from "./actions/hookActions";
 import "./App.css";
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "setSecretWord":
+      return { ...state, secretWord: action.payload };
+    default:
+      throw new Error(`Invalid action type: ${action.type} `);
+  }
+}
+
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+
+  const setSecretWord = (secretWord) =>
+    dispatch({ type: "setSecretWord", payload: secretWord });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
   return <div data-test="componentApp" />;
 }
 
 export default App;
-("");
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,30 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 import { findByTestAttr } from "../test/testUtils";
 import App from "./App";
 
+import hookActions from "./actions/hookActions";
+
+const mockGetSecretWord = jest.fn();
+
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  // use mount because useEffect not called on 'shallow'
+  return mount(<App />);
 };
 
 test("App renders without error", () => {
   const wrapper = setup();
   const componentApp = findByTestAttr(wrapper, "componentApp");
   expect(componentApp.length).toBe(1);
+});
+describe("getSecretWord calls", () => {
+  test("getSecretWord gets called on App mount", () => {
+    setup();
+
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
 });
